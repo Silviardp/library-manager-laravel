2f19e37d2f9174f46322d23b2e84e228b08e3aa2
Silvia Rocha <silviadprocha@gmail.com>
2020-03-13 09:37:25 +0900

    change welcome page to redirect to index view

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/books', 'BookController@index');
-Route::post('/book', 'BookController@store');
+Route::get('/books', 'BookController@index')->name('index');
+Route::post('/book', 'BookController@store')->name('store');
 Route::delete('/book/{book}', 'BookController@destroy');
 
 Route::get('/home', 'HomeController@index')->name('home');
