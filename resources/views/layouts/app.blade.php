bf026a9a0a3a6e67ea1421ffde0a55c0fc15d810
Silvia Rocha <silviadprocha@gmail.com>
2020-03-16 11:05:48 +0900

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Library Manager</title>
+    <title>Ebooky</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
